71e7f6734138c508a9620f372e59d48bc6137aee
Igor <d>
2020-12-12 17:01:45 +0300

    colors

--- BlackStarWear/Extensions/UIColor+Extensions.swift
+++ BlackStarWear/Extensions/UIColor+Extensions.swift
@@ -3,6 +3,40 @@ import UIKit
 
 extension UIColor {
     
+    static var buyButtonColor: UIColor {
+        return UIColor(red: 0.967, green: 0.234, blue: 0.41, alpha: 0.85)
+    }
     
+    static var itemDescriptionTextColor: UIColor {
+        return UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1)
+    }
+    
+    static var addToCartButtonColor: UIColor {
+        return UIColor(red: 0, green: 0.478, blue: 1, alpha: 1)
+    }
+    
+    static var pageControlDotColor: UIColor {
+        return UIColor(red: 0, green: 0.784, blue: 0.325, alpha: 1)
+    }
+    
+    static var makeOrderButtonColor: UIColor {
+        return addToCartButtonColor
+    }
+    
+    static var yesButtonColor: UIColor {
+        return makeOrderButtonColor
+    }
+    
+    static var binViewColor: UIColor {
+        return UIColor(red: 0.62, green: 0.62, blue: 0.62, alpha: 1)
+    }
+    
+    static var binViewItemTextColor: UIColor {
+        return UIColor(red: 0.62, green: 0.62, blue: 0.62, alpha: 1)
+    }
+    
+    static var binCancelButtonColor: UIColor {
+        return UIColor(red: 0.847, green: 0.847, blue: 0.847, alpha: 1)
+    }
     
 }
