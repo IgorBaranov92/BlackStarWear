efae11add8759fa9f19855f815b2907821618118
Igor <d>
2020-12-15 15:25:40 +0300

    scrollView

--- BlackStarWear/Controller/ItemDetailViewController.swift
+++ BlackStarWear/Controller/ItemDetailViewController.swift
@@ -4,30 +4,38 @@ class ItemDetailViewController: UIViewController {
 
     var item: Item!
     
-    @IBOutlet var heightConstraints: [NSLayoutConstraint]!
+    @IBOutlet var spacings: [NSLayoutConstraint]!
     @IBOutlet var outlets: [UIView]!
 
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageScrollView: UIScrollView!
     @IBOutlet weak var priceLabel: PriceLabel!
     @IBOutlet weak var costLabel: CostLabel!
     @IBOutlet weak var itemNameLabel: ItemNameLabel!
     @IBOutlet weak var itemDescriptionLabel: ItemDescriptionLabel!
     @IBOutlet weak var addToCartLabel: AddToCartLabel!
     @IBOutlet weak var addToCartView: AddToCartView!
+    @IBOutlet weak var pageControl: UIPageControl! { didSet {
+        pageControl.numberOfPages = item.images.count
+    }}
+    
+    @IBOutlet private weak var imageHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         scrollView.contentSize = CGSize(width: view.bounds.width, height: 1000)
-        print(scrollView.contentSize)
+        imageScrollView.contentSize = CGSize(width: view.bounds.width * CGFloat(item.images.count), height: imageHeightConstraint.constant)
     }
     
+    
     private var totalHeight: CGFloat {
-        return heightConstraints.reduce(heightConstraints.first!.constant, {$0 + $1.constant} ) + outlets.reduce(outlets.first!.bounds.height, { $0 + $1.bounds.size.height})
+        return spacings.reduce(0, {$0 + $1.constant} ) + outlets.reduce(0, { $0 + $1.bounds.size.height})
     }
     
     @IBAction func back(_ sender: UIButton) {
