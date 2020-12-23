c21829eb1df3f9644702b3364fdfa52ad545b2c3
Igor <d>
2020-12-23 18:50:09 +0300

    final

--- BlackStarWear/Misc/String+Extensions.swift
+++ BlackStarWear/Misc/String+Extensions.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 extension String {
-    public var noHTML: String {
+    var noHTML: String {
         guard let data = self.data(using: .utf8) else { return self }
         let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
             .documentType: NSAttributedString.DocumentType.html,
             .characterEncoding: String.Encoding.utf8.rawValue
         ]
-        guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
-            return self
-        }
-
+        guard let attributedString = try?
+                NSAttributedString(data: data,
+                                   options: options,
+                                   documentAttributes: nil) else { return self }
         return attributedString.string
     }
 }
